f04af591d21ebb2fd6a74c8d3d826225153491cc
Emmett Gaines <ninjanomnom@gmail.com>
2020-09-23 13:04:21 -0400

    Makes plugin order allow plugins that don't care about order

--- ScientiaMagica.PluginLoader/src/PluginOrderComparer.cs
+++ ScientiaMagica.PluginLoader/src/PluginOrderComparer.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Linq;
 using ScientiaMagica.Common.Loader;
 
@@ -38,7 +38,7 @@ namespace ScientiaMagica.PluginLoader {
                 return -1;
             }
 
-            return 0;
+            return -1;
         }
     }
 }
